da39e39b13f04de4c0142e68af6c77bad6f2e441
Felipe Magro <felipemagroo@gmail.com>
2024-10-13 01:46:43 -0300

    feat: btn + text box

--- site-a3-boardgameswizard/src/CaixadeMensagem.js
+++ site-a3-boardgameswizard/src/CaixadeMensagem.js
@@ -3,13 +3,17 @@ import React from 'react'
 // Componente MessageInput
 const CaixaDeMensagem = () => {
     return (
-      <div className='row'>
-        <div class="col-xs-6">
+      <div className="row">
+        <div class="col-md-9 offset-md-3">
           <div className="message-input">
-            <input type="text" placeholder="Digite sua mensagem..." />
+            <textarea class="form-control" placeholder="Digite seu texto" id="caixademensagem"></textarea>
+
+              <div className="button">
+                  <button type="submit" class="btn btn-warning">Enviar</button>
+              </div>
+
           </div>
         </div>
-        <button type="submit">Enviar</button>
       </div>
      
     );
