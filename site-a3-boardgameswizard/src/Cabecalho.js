e9bd2c1daca3028780edd9e8ff73751d1010c46a
vitoriamvrques <vitoria0311@hotmail.com>
2024-10-13 00:31:47 -0300

    altera o design do cabeçalho

--- site-a3-boardgameswizard/src/Cabecalho.js
+++ site-a3-boardgameswizard/src/Cabecalho.js
@@ -4,8 +4,8 @@ import React from "react";
 const Cabecalho = () => {
     return (
       <div className="header">
-        <img src="https://cdn-icons-png.flaticon.com/512/10437/10437838.png" alt="Ícone" className="icon" />
-        <h1>Board Games Wizard</h1>
+        <img src="https://png.pngtree.com/png-clipart/20220816/ourmid/pngtree-black-witch-hat-halloween-element-with-purple-rope-png-image_6112185.png" alt="Ícone" className="icon" />
+        <h1>Board Games Wizard!</h1>
       </div>
     );
   };
